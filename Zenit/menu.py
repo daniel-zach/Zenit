2c322656d5ec046ec1cd16830cac741e8f1b3b2b
João Pedro <joao.pleite@ufrpe.br>
2025-10-15 07:07:53 -0300

    alguns bugs consertados, melhor visual pro menu de gerenciar metas

--- Zenit/menu.py
+++ Zenit/menu.py
@@ -11,7 +11,7 @@ def menu():
         print("[0] Sair do Programa")
         opcao = input("Escolha uma opção: ")
         limpar_terminal()
-         
+
         if opcao == "1":
             menumetas()
         elif opcao == "2":
@@ -20,9 +20,7 @@ def menu():
         elif opcao == "3":
             return
         elif opcao == "0":
-            # FIXME: resolver a questão que as vezes isso não funciona direito (fica preso no while ainda)
             print("Esperamos te ver amanhã!")
-            return
         else:
             print(cores.VERMELHO + "Opção não reconhecida!" + cores.NORMAL)
 
@@ -32,21 +30,18 @@ def menumetas():
         print(cores.VERDE + "Gerenciar Metas \n" + cores.NORMAL)
         print("[C] Criar uma nova meta")
         for x in listametas:
-            # TODO: listar tudo da lista de modo correto igual as outras opções
-            # acho que é bom indexar uns numeros no dict também para poder escolher 
-            # também deixar acessar um menu para todos as metas, incluindo o nome, a descrição, e opções para editar, remover
-            # e marcar a meta como completo para o dia
-            # é complexo, mais acho que vai valer a pena
-            print(x)
+            # TODO: deixar acessar um menu para todos as metas, incluindo o nome, a descrição, e opções para editar, remover
+            # e poder marcar a meta como completo para o dia
+            print("[" + str(listametas[x]['index']) + "] " + str(listametas[x]['nome']))
         print("[0] Voltar para o Menu Principal")
         opcao = input("Escolha uma opção: ")
         limpar_terminal()
 
         if opcao == "c":
             criarmetas()
-        if opcao == "0":
-            menu()
+        elif opcao == "0":
+            return
         else:
-            # FIXME: isso está sendo ativado após ir para o menu de criar metas
             print(cores.VERMELHO + "Opção não reconhecida!" + cores.NORMAL)
+
 menu()
--- Zenit/metas.py
+++ Zenit/metas.py
@@ -1,16 +1,21 @@
 from util import limpar_terminal
-listametas = {}
+listametas = dict()
 
 # falta muita coisa aqui ainda mas essa é a base
 # escolha o nome, a descrição da meta (talvez depois a dificuldade e o tempo? vamos ver)
 # e estará adicionada a lista
+# FIXME: não é bloqueado a criação de metas com o mesmo nome, mas trás resultados não desejados.
+# deve ser criado o bloqueio para apenas metas com o mesmo nome serem criados
 def criarmetas():
     limpar_terminal()
     nome = input("Defina o nome da meta: ")
     descricao = input("Defina a descrição da meta: ")
+    index = len(listametas) + 1
     novameta = {
         "nome": nome,
-        "descrição": descricao
+        "descrição": descricao,
+        "index": index
     }
-    listametas.update({str(nome) : novameta})
+    listametas.update({str(index) : novameta})
     limpar_terminal()
+    print(listametas) # só por questões de teste
