97b8941c1e22a16a55484a03e9286b8178c849eb
João Pedro <joao.pleite@ufrpe.br>
2025-10-18 09:09:04 -0300

    sistema de missões

--- Zenit/menu.py
+++ Zenit/menu.py
@@ -1,5 +1,5 @@
 from util import cores, limpar_terminal
-from metas import criarmetas, listametas
+from metas import criarmetas, listametas, criarmissao
 
 def menu():
     opcao = ''
@@ -29,8 +29,6 @@ def menumetas():
         print(cores.VERDE + "Gerenciar Metas \n" + cores.NORMAL)
         print("[C] Criar uma nova meta")
         for x in listametas:
-            # TODO: deixar acessar um menu para todos as metas, incluindo o nome, a descrição, e opções para editar, remover
-            # e poder marcar a meta como completo para o dia
             print("[" + str(listametas[x]['index']) + "] " + str(listametas[x]['nome']))
         print("[0] Voltar para o Menu Principal")
         opcao = input("Escolha uma opção: ").strip().lower()
@@ -51,16 +49,15 @@ def menuvizualizarmeta(index):
     while opcao != "0":
         print("Nome da meta: " + listametas[index]["nome"])
         print("Descrição da meta: " + listametas[index]["descrição"] + "\n")
-        print("[1] Marcar como completa")
+        print("[1] Missões")
         print("[2] Editar meta")
         print("[3] Excluir meta")
-        print("[0] Voltar ao menu metas")
+        print("[0] Voltar ao menu de metas")
         opcao = input("Escolha uma opção: ")
         limpar_terminal()
 
         if opcao == "1":
-            # TODO: funcionalidade de marcar como completa
-            return
+            menumissoes(index)
         elif opcao == "2":
             opcao = input("Editar nome ou descrição? ")
             if opcao.casefold() == "nome":
@@ -72,9 +69,54 @@ def menuvizualizarmeta(index):
             else:
                 print(cores.VERMELHO + "Opção não reconhecida!" + cores.NORMAL)
             limpar_terminal()
-            
         elif opcao == "3":
             listametas.pop(index)
+            return
+        elif opcao == "0":
+            return
+        else:
+            print(cores.VERMELHO + "Opção não reconhecida!" + cores.NORMAL)
+
+def menumissoes(index):
+    opcao = ''
+    while opcao != "0":
+        print("Missões de " + listametas[index]["nome"] + "\n")
+        print("[C] Criar uma nova missão")
+        for i in listametas[index]['missoes']:
+            print("[" + str(listametas[index]['missoes'][i]['index']) + "] " + str(listametas[index]['missoes'][i]['nome']))
+        print("[0] Voltar ao menu da meta " + listametas[index]["nome"])
+        opcao = input("Escolha uma opção: ")
+        limpar_terminal()
+        if opcao.casefold() == "c":
+            criarmissao(index)
+        elif opcao == "0":
+            return
+        else:
+            if opcao in listametas[index]['missoes']:
+                menuvizualizarmissao(index, opcao)
+            else:
+                print(cores.VERMELHO + "Opção não reconhecida!" + cores.NORMAL)
+
+def menuvizualizarmissao(metaindex, missaoindex):
+    opcao = ''
+    while opcao != "0":
+        print("Nome da missão: " + listametas[metaindex]['missoes'][missaoindex]["nome"])
+        print("[1] Marcar como completa")
+        print("[2] Editar missão")
+        print("[3] Excluir missão")
+        print("[0] Voltar ao menu de missões")
+        opcao = input("Escolha uma opção: ")
+        limpar_terminal()
+
+        if opcao == "1":
+            listametas[metaindex]['missoes'][missaoindex]["completa"] = True
+            # TODO: integrar com o sistema de streaks, resetar isso para false em 24 horas
+        elif opcao == "2":
+                x = input("Define o nome da meta: ")
+                listametas[metaindex]['missoes'][missaoindex]["nome"] = x
+                limpar_terminal()
+        elif opcao == "3":
+            listametas[metaindex]['missoes'].pop(missaoindex)
             return
         elif opcao == "0":
             return
--- Zenit/metas.py
+++ Zenit/metas.py
@@ -1,20 +1,34 @@
 from util import limpar_terminal
 listametas = dict()
 
-# falta muita coisa aqui ainda mas essa é a base
-# escolha o nome, a descrição da meta (talvez depois a dificuldade e o tempo? vamos ver)
-# e estará adicionada a lista
-# FIXME: não é bloqueado a criação de metas com o mesmo nome, mas trás resultados não desejados.
-# deve ser criado o bloqueio para apenas metas com o mesmo nome serem criados
 def criarmetas():
     limpar_terminal()
     nome = input("Defina o nome da meta: ")
+    if not nome:
+        criarmetas()
+        return
     descricao = input("Defina a descrição da meta: ")
     index = len(listametas) + 1
     novameta = {
         "nome": nome,
         "descrição": descricao,
-        "index": index
+        "index": index,
+        "missoes": {}
     }
     listametas.update({str(index) : novameta})
+    limpar_terminal()
+
+def criarmissao(x):
+    limpar_terminal()
+    nome = input("Defina o nome da missão: ")
+    if not nome:
+        criarmissao(x)
+        return
+    index = len(listametas[x]["missoes"]) + 1
+    novamissao = {
+        "nome": nome,
+        "index": index,
+        "completa": False
+    }
+    listametas[x]["missoes"].update({str(index) : novamissao})
     limpar_terminal()
